37705507974f9fa015861d5defa98868c8f29fcd
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-04-30 10:21:43 -0500

    [MOD, login.js][ADD, setNav_setModal]

--- public/js/users/login.js
+++ public/js/users/login.js
@@ -4,11 +4,17 @@ let usersRef = db.collection("users");
 
 // variables
 let btnLogin = document.getElementById('btnLogin');
+let btnSignOut = document.getElementById('btnSignOut');
+let clickLogin = document.getElementById('clickLogin');
 let user;
 
 // initialize app
-let initial = () => {
+let initializeLogin = () => {
 	btnLogin.addEventListener('click', loginWithGoogle, false);
+	btnSignOut.addEventListener('click', leave, false);
+	$('#alertSignOut').hide();
+	$('#modalTextLogout').hide();
+	$('#modalTextLogout2').hide();
 };
 
 // current user
@@ -29,6 +35,7 @@ let loginWithGoogle = () => {
 			user = result.user;
 			createUser(user);
 			setUser(user);
+			setModal();
 		}).catch((error) => {
 			console.log(error);
 		})
@@ -39,37 +46,38 @@ let setUser = (user) => {
 	if (!user) {
 		document.querySelector('#loginButtonWrapper').innerHTML = `
 			<button class="btn" id="btnLogin" type="btn">Iniciar Sesión</button>
-			<img class="imgAvatar" src="img/ardulink.png" alt="UserAvatar" />
+			<img class="imgAvatar" src="../img/ardulink.png" alt="UserAvatar" />
 		`
+		$('#logOutNav').hide(1000);
 		let btnLogin = document.getElementById('btnLogin');
 		btnLogin.addEventListener('click', function () {
 			loginWithGoogle();
 		})
 	} else {
+		document.querySelector('#logOutNav').innerHTML = `
+			<a class="nav-link" data-bs-toggle="modal" data-bs-target="#signOutModal" href="#">Cancelar Suscripción</a>
+		`
+		$('#logOutNav').show(2000);
 		document.querySelector('#loginButtonWrapper').innerHTML = `
-		<button class="btn" id="btnLogOut" type="btn">Cerrar Sesión</button>
-		<button class="btn" id="btnSignOut" type="btn">Cancelar Suscripción</button>
-		<img class="imgAvatar" src="${user.photoURL}" alt="UserAvatar" />
+			<button class="btn" id="btnLogOut" type="btn">Cerrar Sesión</button>
+			<img class="imgAvatar" src="${user.photoURL}" alt="UserAvatar" />
 		`
 		let btnLogOut = document.getElementById('btnLogOut');
-		let btnSignOut = document.getElementById('btnSignOut');
 		btnLogOut.addEventListener('click', function () {
 			logOut();
-		})
-		btnSignOut.addEventListener('click', function () {
-			// signOut();
-			leave(user);
 		})
 	}
 };
 
 // create User
 let createUser = (user) => {
+	let userCreationDate = new Date();
 	usersRef.doc(user.uid).set({
 		name: user.displayName,
 		email: user.email,
 		image: user.photoURL,
-		userId: user.uid
+		userId: user.uid,
+		date: userCreationDate
 	})
 		.then(() => {
 			console.log("user created!");
@@ -91,8 +99,9 @@ let logOut = () => {
 };
 
 // delete user
-let leave = (userDel) => {
-	usersRef.doc(userDel.uid)
+let leave = () => {
+	let userSup = firebase.auth().currentUser;
+	usersRef.doc(userSup.uid)
 		.delete()
 		.then(() => {
 			console.log("User Deleted");
@@ -109,11 +118,41 @@ let signOut = () => {
 	userDel.delete().then(function () {
 		user = undefined;
 		setUser(user);
-		// leave(userDel);
+		setNav();
 		console.log("User SignOut");
 	}).catch(function (error) {
 		console.log(error);
 	});
 };
 
-window.addEventListener(`load`, initial, false);
+// after signOut
+let setNav = () => {
+	$('#alertSignOut').show(1000);
+	$('#modalTextLogin').hide(1000);
+	$('#modalTextLogin2').hide(1000);
+	$('#modalTextLogout').show(3000);
+	$('#modalTextLogout2').show(3000);
+	$('#btnSignOut').hide(2000);
+	document.querySelector('#logOutNav').innerHTML = `
+		<a class="nav-link" id="clickLogin" href="#">
+			Registrar
+		</a>
+	`
+	let clickLogin = document.getElementById('clickLogin');
+	clickLogin.addEventListener('click', function () {
+		loginWithGoogle();
+	})
+};
+
+// set modal
+let setModal = () => {
+	$('#alertSignOut').hide();
+	$('#modalTextLogin').show();
+	$('#modalTextLogin2').show();
+	$('#modalTextLogout').hide();
+	$('#modalTextLogout2').hide();
+	$('#btnSignOut').show();
+	console.log("set modal");
+}
+
+window.addEventListener(`load`, initializeLogin, false);
