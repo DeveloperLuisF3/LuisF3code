ab34ca53107d2902ac8a2408f52375f6944038ba
Developer LuisF3 <developerluisfernando3@gmail.com>
2021-12-03 11:20:58 -0600

    [MOD, adminHtml.js][ADD, sección_editProjrct]

--- public/js/adminPages/aHtml/adminHtml.js
+++ public/js/adminPages/aHtml/adminHtml.js
@@ -18,6 +18,8 @@ let imgHtml = document.getElementById('img');
 let goToImgGallery = document.getElementById('goToImgGallery');
 let setModalHtml = document.getElementById('setModalHtml');
 let submitProjectHtml = document.getElementById('submitProjectHtml');
+let editImage = document.getElementById('editImage');
+let exitUpdateForm = document.getElementById('exitUpdateForm');
 let htmlImgItems;
 let files, fl, i, file;
 
@@ -35,6 +37,8 @@ let initializeAdminHtml = () => {
 	goToImgGallery.addEventListener('click', showImgWrapper, false);
 	setModalHtml.addEventListener('click', showModalHtml, false);
 	submitProjectHtml.addEventListener('click', getHtmlData, false);
+	editImage.addEventListener('click', showImgWrapper, false);
+	exitUpdateForm.addEventListener('click', hideUpdateHtml, false);
 	$('#imgWrapper').hide();
 	$('#closeImgWrapper').hide();
 	$('#imgPortWrapper').hide();
@@ -50,9 +54,22 @@ let initializeAdminHtml = () => {
 	$('#alertImgForm').hide();
 	$('#alertImgHtml').hide();
 	$('#alertCreateHtml').hide();
+	$('#alertEditImg').hide();
+	$('#alertEditImg2').hide();
+	$('#alertUpdateHtml').hide();
+	$('#alertDeleteHtml').hide();
 };
 
-// current image
+// listener projectHtml
+let statusProjectHtml = () => {
+	projectHtml.onSnapshot((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			listProjectHtml([doc.data()]);
+		});
+	});
+};
+
+// listener imageHtml
 let htmlImageCurrentState = () => {
 	imageHtmlRef.onSnapshot((querySnapshot) => {
 		querySnapshot.forEach((doc) => {
@@ -61,6 +78,85 @@ let htmlImageCurrentState = () => {
 	});
 };
 
+let listProjectHtml = (arrayHtml) => {
+	arrayHtml.forEach((value, index) => {
+		console.log(value, "=>", index);
+		$('#projectHtmlWrapper').append(
+			`
+				<li class="list-group-item" id="${index}">
+					<div class="data-wrapper" id="${value.date}">
+						<p>id: ${value.name}</p>
+						<p>${value.name}</p>
+						<p>${value.technology}</p>
+						<p>${value.description}</p>
+						<p>${value.documentation}</p>
+						<p>${value.video}</p>
+						<p>${value.image}</p>
+						<a href="#updateFormHtml">
+							<button type="button" class="btn edit" onclick="editProjectHtml('${index}', '${value.name}', '${value.technology}', '${value.description}', '${value.documentation}', '${value.video}', '${value.image}')">
+								Update
+							</button>
+						</a>
+						<button type="button" class="btn delete" data-bs-toggle="modal" data-bs-target="#modalDeleteHtml" onclick="modalRemoveHtml('${value.name}',)">
+							Delete
+						</button>
+					</div>
+				</li>
+			`
+		);
+	});
+};
+
+let sendImageToEdit = (url) => {
+	$('#htmlEditImage').empty();
+	hideImgWrapper();
+	$('#htmlEditImage').val(url);
+	$('#alertEditImg').show(2000);
+	$('#alertEditImg').hide(4000);
+	$('#alertEditImg2').show(2000);
+	$('#alertEditImg2').hide(4000);
+};
+
+// edit project html
+let editProjectHtml = (index, name, technology, description, documentation, video, image) => {
+	$('#submitUpdateHtml').show();
+	$('#updateFormHtml').show();
+	$('#idProjectHtml').empty();
+	$('#idProjectHtml').append(
+		`
+			<h4 class="id">
+				${name} ==> ${index}
+			</h4>
+		`
+	);
+	$('#htmlEditName').val(name);
+	$('#htmlEditTech').val(technology);
+	$('#htmlEditDesc').val(description);
+	$('#htmlEditDoc').val(documentation);
+	$('#htmlEditVideo').val(video);
+	$('#htmlEditImage').val(image);
+	$('#submitUpdateHtml').click(() => {
+		var editHtmlRef = projectHtml.doc(name);
+		return editHtmlRef.update({
+			name: document.getElementById('htmlEditName').value,
+			technology: document.getElementById('htmlEditTech').value,
+			description: document.getElementById('htmlEditDesc').value,
+			documentation: document.getElementById('htmlEditDoc').value,
+			video: document.getElementById('htmlEditVideo').value,
+			image: document.getElementById('htmlEditImage').value
+		}).then(() => {
+			$('#submitUpdateHtml').hide(2000);
+			$('#alertUpdateHtml').show(2000);
+			$('#alertUpdateHtml').hide(4000);
+			hideUpdateHtml(2000);
+			hideListHtml(4000);
+		}).catch((error) => {
+			console.log("Error updating document: ", error);
+		});
+	});
+};
+
+// add image to projectHTML form
 let sendImgToProject = (url) => {
 	$('#htmlProjectImage').empty();
 	showFormHtml();
@@ -72,7 +168,10 @@ let sendImgToProject = (url) => {
 	hideImgWrapper();
 };
 
+// add projectHtml to firebase
 let getHtmlData = () => {
+	$('#projectHtmlWrapper').empty();
+	$('#proCreated').show();
 	let dateHtml = new Date();
 	let nameHtml = document.getElementById('htmlProjectName').value;
 	let technologyHtml = document.getElementById('htmlProjectTech').value;
@@ -89,6 +188,7 @@ let getHtmlData = () => {
 		video: videoHtml,
 		image: imageHtml
 	}).then(() => {
+		hideListHtml();
 		$("input").val("");
 		$("textarea").val("");
 		$('#alertCreateHtml').show(2000);
@@ -97,13 +197,49 @@ let getHtmlData = () => {
 		$('#submitProjectHtml').hide(2000);
 		$("#modalBodyHtml").append(
 			`
-        		<p id="proCreat">PRODUCTO CREADO!!!</p>
+        		<p id="proCreated">PROYECTO CREADO!!!</p>
     		`
 		);
-		document.getElementById("proCreat").style.color = '#81c784';
+		document.getElementById("proCreated").style.color = '#81c784';
 		hideFormHtml();
 	}).catch((error) => {
 		console.log("Error writing document: ", error);
+	});
+};
+
+// pass data to delete projectHtml
+let modalRemoveHtml = (n) => {
+	$('#proCreated').hide();
+	$('#textModalDelete').empty();
+	$('#textModalDelete').append(
+		`
+			Estás seguro de eliminar este producto ==> ${n} ???
+		`
+	);
+	$('#deleteHtmlFooter').empty();
+	$('#deleteHtmlFooter').append(
+		`
+			<button type="submit" class="btn modal-signOut" id="submitDeleteHtml" onclick="deleteHtmlProject('${n}')">
+				Delete
+			</button>
+			<button type="button" class="btn modal-cancel" data-bs-dismiss="modal">
+				Cancelar
+			</button>
+		`
+	);
+	$('#submitDeleteHtml').show();
+};
+
+// delete projectHtml
+let deleteHtmlProject = (itemDelete) => {
+	projectHtml.doc(itemDelete).delete().then(() => {
+		console.log("Document successfully deleted!");
+		hideListHtml();
+		$('#alertDeleteHtml').show(2000);
+		$('#alertDeleteHtml').hide(4000);
+		$('#submitDeleteHtml').hide(2000);
+	}).catch((error) => {
+		console.log("Error removing document: ", error);
 	});
 };
 
@@ -181,9 +317,7 @@ let crearNodoEnBD = (name, downloadURL) => {
 
 // image container fill
 let presentHtmlImg = (array) => {
-	console.log(array);
 	array.forEach((el, index) => {
-		console.log(el, "=>", index);
 		$("#imgWrapper").append(
 			`
 				<div class="col-6 col-sm-4 col-md-3 col-lg-2" id="${index}" >
@@ -194,9 +328,16 @@ let presentHtmlImg = (array) => {
 							<small class="title">${el.nombre}</small>
 						</div>
 						<ul class="list-group list-group-flush img-wrapper-btn">
-							<a href="#uploadProject"><button type="button" class="btn carry-send" onclick="sendImgToProject('${el.url}')">
-								Send to
-							</button></a>
+							<a href="#uploadProject">
+								<button type="button" class="btn carry-send" onclick="sendImgToProject('${el.url}')">
+									Send to
+								</button>
+							</a>
+							<a href="#htmlEditImage">
+								<button type="button" class="btn carry-edit" onclick="sendImageToEdit('${el.url}')">
+									Edit
+								</button>
+							</a>
 							<button type="button" class="btn del-img" data-bs-toggle="modal" data-bs-target="#modalElimImg"  onclick="passDataToDelete('${el.nombre}', '${index}')">
 								Delete
 							</button>
@@ -227,7 +368,7 @@ let passDataToDelete = (nombre, index) => {
 				Cancelar
 			</button>
 		`
-	)
+	);
 };
 
 // remove image from storage
@@ -304,17 +445,24 @@ let hideFormHtml = () => {
 	$('#openFormHtml').show(3000);
 };
 
+// close updateForm
+let hideUpdateHtml = () => {
+	$('#updateFormHtml').hide(2000);
+};
+
 // open listHtml
 let showListHtml = () => {
 	$('#openListHtml').hide(1000);
 	$('#projectListHtml').show(2000);
 	$('#closeListHtml').show(3000);
+	statusProjectHtml();
 };
 // close listHtml
 let hideListHtml = () => {
 	$('#closeListHtml').hide(1000);
 	$('#projectListHtml').hide(2000);
 	$('#openListHtml').show(3000);
+	$('#projectHtmlWrapper').empty();
 };
 
 // show modal create project html
